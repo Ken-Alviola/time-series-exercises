8bb8a19161ead7efa3c5650033d910019adb50a9
Ken-Alviola <kenalviola@gmail.com>
2021-04-14 10:39:06 -0500

    comments

--- acquire.py
+++ acquire.py
@@ -1,24 +1,29 @@
 import pandas as pd 
 import requests
 
-def get_zach(page):
+def get_zach(endpoint):
     
-    '''input string is a page name. Function will return a dataframe filled the items in the key specified'''
+    '''input string the endpoint of the url. Function will return a dataframe filled with items in the key specified (items, sales, stores, etc)'''
     
+    #base + endpoint
     base_url = 'https://python.zach.lol/api/v1/'
-    url = base_url + page
+    url = base_url + endpoint
     
+    #reading json and getting max page
     response = requests.get(url)
     data = response.json()
     n = data['payload']['max_page']
     
+    #new list for the loop
     new_list = []
     
+    #looping through all pages and pulling data
     for i in range(1, n+1):
         new_url = url + '?page=' + str(i)
         response = requests.get(new_url)
         data = response.json()
-        page_items = data['payload'][page]
+        page_items = data['payload'][endpoint]
         new_list += page_items
     
+    #returns a dataframe of the list items from the loop
     return pd.DataFrame(new_list)
